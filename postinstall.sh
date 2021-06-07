1551abbe0be9922178010c5dca766b976e78d083
Jared Neil <jaredneil@lucidchart.com>
2021-06-07 10:36:10 -0600

    Add patches

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -xe
+patch-package
 ngcc --tsconfig closure.tsconfig.json --loglevel debug --no-async
 find node_modules/@angular node_modules/ngx-monaco-editor -name '*.js' -o -name '*.ts' -exec sed -i -r 's:^//# sourceMappingURL=.*::' {} \;
