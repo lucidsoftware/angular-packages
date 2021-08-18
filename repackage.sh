6c165dd3b7e4def5a7d9843ed1a1a02625b712b4
David Neil <davidneil@lucidchart.com>
2021-08-18 15:38:33 -0600

    Output reproducible tgz bundles

--- repackage.sh
+++ repackage.sh
@@ -19,8 +19,7 @@ mkdir -p dist
 
 for p in "${PACKAGES[@]}"; do
   echo "$p"
-
   package=${p//\//_} # Replace / with _
   package=${package/@/} # Replace @ with nothing (@ is interpreted as a version delimiter in Yarn2)
-  tar --create --gzip --file "dist/${package}.tgz" --directory "node_modules/${p}" --transform 's:^\.:package:' .
+  tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2019-01-01' --create --directory "node_modules/${p}" --transform 's:^\.:package:' . | gzip -n > "dist/${package}.tgz"
 done
