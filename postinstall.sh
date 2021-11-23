89c6d41238426cbd29375c9dad470732582adab2
David Neil <davidneil@lucidchart.com>
2021-11-23 13:54:03 -0700

    Upgrade to Angular 13.0.2

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -xe
 patch-package
-ngcc --tsconfig closure.tsconfig.json --loglevel debug --no-async
-find node_modules/@angular node_modules/ngx-monaco-editor -name '*.js' -o -name '*.ts' -exec sed -i -r 's:^//# sourceMappingURL=.*::' {} \;
