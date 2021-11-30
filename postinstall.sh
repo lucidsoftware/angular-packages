2ae84f2e158b2812e98a6023aea1aab4d2d06ac2
David Neil <davidneil@lucidchart.com>
2021-11-29 21:15:19 -0700

    Re-add ngcc for ngx-monaco-editor
    We are using a very old version of ngx-monaco-editor,
    so it still requires ngcc.

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -xe
 patch-package
+ngcc --tsconfig closure.tsconfig.json --target ngx-monaco-editor --loglevel debug --no-async
+find node_modules/ngx-monaco-editor -name '*.js' -o -name '*.ts' -exec sed -i -r 's:^//# sourceMappingURL=.*::' {} \;
