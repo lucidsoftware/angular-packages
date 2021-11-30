5da7a24aea281803eb45b95bd800a4ac1ece3a6e
David Neil <davidneil@lucidchart.com>
2021-11-29 17:45:33 -0700

    Remove @angular/animations

--- repackage.sh
+++ repackage.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 PACKAGES=(
-  "@angular/animations"
   "@angular/common"
   "@angular/compiler-cli"
   "@angular/compiler"
@@ -18,7 +17,7 @@ PACKAGES=(
 mkdir -p dist
 
 for p in "${PACKAGES[@]}"; do
-  echo "$p"
+  echo "Packaging" "$p"
   package=${p//\//_} # Replace / with _
   package=${package/@/} # Replace @ with nothing (@ is interpreted as a version delimiter in Yarn2)
   tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2019-01-01' --create --directory "node_modules/${p}" --transform 's:^\.:package:' . | gzip -n > "dist/${package}.tgz"
