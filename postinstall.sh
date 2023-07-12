3068522f08f89d97c853a49ff9fe0c6b4a0cfb11
Jake Tyler <jtyler@lucidchart.com>
2023-07-12 10:43:55 -0600

    Upgrade to Angular 16.1.2

--- postinstall.sh
+++ postinstall.sh
@@ -3,11 +3,10 @@ set -xe
 patch-package
 # COMMENT EVERYTHING BELOW THIS LINE BEFORE CHANGING PATCHES
 # Remove unused packages and files
-find node_modules/@angular/**/esm2020 -maxdepth 0 -type d -exec rm -r {} \;
-find node_modules/@angular/**/fesm2020 -maxdepth 0 -type d -exec rm -r {} \;
+find node_modules/@angular/**/esm2022 -maxdepth 0 -type d -exec rm -r {} \;
 find node_modules/@angular/ -name '*.map' -delete
 # Run Angular's Babel linker to produce AoT output
-find node_modules/@angular/**/fesm2015 -name '*.mjs' -exec node linker/linker.mjs {} {} \;
+find node_modules/@angular/**/fesm2022 -name '*.mjs' -exec node linker/linker.mjs {} {} \;
 # Run ngcc for pre APF13 packages
 ngcc --tsconfig closure.tsconfig.json --target ngx-monaco-editor --loglevel debug --no-async
 # Remove source maps to decrease size and increase reproducibility
